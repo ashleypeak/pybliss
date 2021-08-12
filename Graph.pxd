086048fefc45049efad3e8720882e5199488ea76
Wyatt Peak <wyattpeak@gmail.com>
2021-08-12 23:25:43 +1000

    Build Cython wrappers for most classes
    Since I'm getting a segfault when wrapping Graph as a whole, trying to
    wrap each class individually to see where it fails. Done everything
    except the various Graph classes and Partition.

--- Graph.pxd
+++ Graph.pxd
@@ -16,39 +16,67 @@ cdef extern from "bliss-0.73/defs.cc":
     pass
 
 cdef extern from "bliss-0.73/defs.hh" namespace "bliss":
-    cdef void fatal_error(char* fmt)
-    cdef void fatal_error(char* fmt, char*, int)
+    cdef void fatal_error(const char* fmt)
+    cdef void fatal_error(const char* fmt, const char*, const int)
 
-cdef extern from "bliss-0.73/graph.cc":
-    pass
+# cdef extern from "bliss-0.73/graph.cc":
+#     pass
 
-cdef extern from "bliss-0.73/graph.hh" namespace "bliss":
-    cdef cppclass Graph:
-        Graph(unsigned int) except +
-        unsigned int add_vertex(unsigned int color)
-        void add_edge(unsigned int v1, unsigned int v2)
-        void change_color(unsigned int vertex, unsigned int color)
+# cdef extern from "bliss-0.73/graph.hh" namespace "bliss":
+#     cdef cppclass Graph:
+#         Graph(unsigned int) except +
+#         unsigned int add_vertex(unsigned int color)
+#         void add_edge(unsigned int v1, unsigned int v2)
+#         void change_color(unsigned int vertex, unsigned int color)
 
 cdef extern from "bliss-0.73/heap.cc":
     pass
 
 cdef extern from "bliss-0.73/heap.hh" namespace "bliss":
     cdef cppclass Heap:
+        Heap() except +
+        void init(const unsigned int size)
+        const bint is_empty()
+        void clear()
+        void insert(const unsigned int e)
         unsigned int remove()
+        const unsigned int size()
+
+cdef extern from "bliss-0.73/kqueue.hh" namespace "bliss":
+    cdef cppclass KQueue[T]:
+        KQueue()
+        void init(const unsigned int N)
+        const bint is_empty()
+        const unsigned int size()
+        void clear()
+        const T front()
+        T pop_front()
+        void push_front(T e)
+        # T pop_back()  # Doesn't seem to be defined, bug?
+        void push_back(T e)
 
 cdef extern from "bliss-0.73/orbit.cc":
     pass
 
 cdef extern from "bliss-0.73/orbit.hh" namespace "bliss":
     cdef cppclass Orbit:
+        Orbit() except +
+        void init(const unsigned int N)
         void reset()
+        void merge_orbits(unsigned int e1, unsigned int e2)
+        const bint is_minimal_representative(unsigned int e)
+        const unsigned int get_minimal_representative(unsigned int e)
+        const unsigned int orbit_size(unsigned int e)
+        const unsigned int nof_orbits()
 
-cdef extern from "bliss-0.73/partition.cc":
-    pass
+# cdef extern from "bliss-0.73/partition.cc":
+#     pass
 
-cdef extern from "bliss-0.73/partition.hh" namespace "bliss":
-    cdef cppclass Partition:
-        void init(unsigned int N)
+# cdef extern from "bliss-0.73/partition.hh" namespace "bliss":
+#     cdef cppclass Partition:
+#         cppclass Cell:
+#             pass
+#         void init(unsigned int N)
 
 cdef extern from "bliss-0.73/timer.cc":
     pass
@@ -56,17 +84,24 @@ cdef extern from "bliss-0.73/timer.cc":
 cdef extern from "bliss-0.73/timer.hh" namespace "bliss":
     cdef cppclass Timer:
         Timer() except +
+        void reset()
+        double get_duration()
 
 cdef extern from "bliss-0.73/uintseqhash.cc":
     pass
 
 cdef extern from "bliss-0.73/uintseqhash.hh" namespace "bliss":
     cdef cppclass UintSeqHash:
+        UintSeqHash() except +
+        UintSeqHash(const UintSeqHash& other) except +
+        # UintSeqHash& operator=(const UintSeqHash& other)
         void update(unsigned int n)
+        const unsigned int get_value()
+        int cmp(const UintSeqHash& other)
 
 cdef extern from "bliss-0.73/utils.cc":
     pass
 
 cdef extern from "bliss-0.73/utils.hh" namespace "bliss":
-    cdef bint is_permutation(unsigned int N, unsigned int* perm)
-    cdef bint is_permutation(vector[unsigned int]& perm)
+    cdef bint is_permutation(const unsigned int N, const unsigned int* perm)
+    cdef bint is_permutation(const vector[unsigned int]& perm)
--- bliss.pyx
+++ bliss.pyx
@@ -1,21 +1,169 @@
-#distutils: language = c++
+# distutils: language = c++
 
-from Graph cimport Graph
+# from Graph cimport vector
 
-cdef class PyGraph:
-    cdef Graph* c_graph
+# cdef class PyVector:
+#     cdef vector[int] c_vector
 
-    def __cinit__(self, int N=0):
-        self.c_graph = new Graph(N)
+#     def push_back(self, int i):
+#         self.c_vector.push_back(i)
 
-    def __dealloc__(self):
-        del self.c_graph
+#     def at(self, int index):
+#         return self.c_vector[index]
 
-    def add_vertex(self, int color=0):
-        self.c_graph.add_vertex(color)
+# from Graph cimport is_permutation
 
-    def add_edge(self, int v1, int v2):
-        self.c_graph.add_edge(v1, v2)
+# cpdef bint is_perm(lst):
+#     # cdef vector[unsigned int] c_vector
+#     # for el in lst:
+#     #     c_vector.push_back(el)
 
-    def change_color(self, int vertex, int color):
-        self.c_graph.change_color(vertex, color)
+#     # return is_permutation(c_vector)
+#     cdef unsigned int c_int_list[5]
+
+#     for (i, el) in enumerate(lst):
+#         c_int_list[i] = el
+
+#     return is_permutation(len(lst), c_int_list)
+
+# from Graph cimport fatal_error
+
+# cpdef void ferror():
+#     cdef char* fmt = "FATAL ERRAR %s:%d"
+#     cdef char* fl = "test.c"
+#     cdef int ln = 3
+#     fatal_error(fmt, fl, ln)
+
+# from cython.operator import dereference
+# from Graph cimport UintSeqHash
+
+# cdef class PyUintSeqHash:
+#     cdef UintSeqHash* c_uintseqhash
+
+#     def __cinit__(self, PyUintSeqHash base=None):
+#         if base is None:
+#             self.c_uintseqhash = new UintSeqHash()
+#         else:
+#             self.c_uintseqhash = new UintSeqHash(dereference(base.c_uintseqhash))
+
+#     def __dealloc__(self):
+#         del self.c_uintseqhash
+
+#     def update(self, unsigned int n):
+#         self.c_uintseqhash.update(n)
+
+#     def get_value(self):
+#         return self.c_uintseqhash.get_value()
+
+#     def compare(self, PyUintSeqHash other):
+#         return self.c_uintseqhash.cmp(dereference(other.c_uintseqhash))
+
+# from Graph cimport Timer
+
+# cdef class PyTimer:
+#     cdef Timer c_timer
+
+#     def reset(self):
+#         self.c_timer.reset()
+
+#     def get_duration(self):
+#         return self.c_timer.get_duration()
+
+# from Graph cimport Orbit
+
+# cdef class PyOrbit:
+#     cdef Orbit c_orbit
+
+#     def init(self, const unsigned int n):
+#         self.c_orbit.init(n)
+
+#     def reset(self):
+#         self.c_orbit.reset()
+
+#     def merge_orbits(self, unsigned int e1, unsigned int e2):
+#         self.c_orbit.merge_orbits(e1, e2)
+
+#     def is_minimal_representative(self, unsigned int e):
+#         return self.c_orbit.is_minimal_representative(e)
+
+#     def get_minimal_representative(self, unsigned int e):
+#         return self.c_orbit.get_minimal_representative(e)
+
+#     def orbit_size(self, unsigned int e):
+#         return self.c_orbit.orbit_size(e)
+
+#     def nof_orbits(self):
+#         return self.c_orbit.nof_orbits()
+
+# from Graph cimport Heap
+
+# cdef class PyHeap:
+#     cdef Heap c_heap
+
+#     def init(self, const unsigned int size):
+#         self.c_heap.init(size)
+
+#     def is_empty(self):
+#         return self.c_heap.is_empty()
+
+#     def clear(self):
+#         self.c_heap.clear()
+
+#     def insert(self, const unsigned int e):
+#         self.c_heap.insert(e)
+
+#     def remove(self):
+#         return self.c_heap.remove()
+
+#     def size(self):
+#         return self.c_heap.size()
+
+from Graph cimport KQueue
+
+cdef class PyKQueue:
+    cdef KQueue[int] c_kqueue
+
+    def init(self, const unsigned int N):
+        self.c_kqueue.init(N)
+
+    def is_empty(self):
+        return self.c_kqueue.is_empty()
+
+    def size(self):
+        return self.c_kqueue.size()
+
+    def clear(self):
+        self.c_kqueue.clear()
+
+    def front(self):
+        return self.c_kqueue.front()
+
+    def pop_front(self):
+        return self.c_kqueue.pop_front()
+
+    def push_front(self, int e):
+        self.c_kqueue.push_front(e)
+
+    def push_back(self, int e):
+        self.c_kqueue.push_back(e)
+
+
+# from Graph cimport Graph
+
+# cdef class PyGraph:
+#     cdef Graph* c_graph
+
+#     def __cinit__(self, int N=0):
+#         self.c_graph = new Graph(N)
+
+#     def __dealloc__(self):
+#         del self.c_graph
+
+#     def add_vertex(self, int color=0):
+#         self.c_graph.add_vertex(color)
+
+#     def add_edge(self, int v1, int v2):
+#         self.c_graph.add_edge(v1, v2)
+
+#     def change_color(self, int vertex, int color):
+#         self.c_graph.change_color(vertex, color)
